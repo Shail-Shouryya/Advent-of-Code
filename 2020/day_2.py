f286a1aca3fb31c58e76b55a33e314512bd24e87
Shail Shouryya <shailshouryya@gmail.com>
2020-12-11 19:09:40 -0800

    Implement day 2 part 1 (text processing) python solution

--- 2020/day_2.py
+++ 2020/day_2.py
@@ -24,3 +24,56 @@ To begin, get your puzzle input.
 Answer:
 ____________
 '''
+def main():
+    text      = read_puzzle_input('puzzle_day_2.txt')
+    all_lines = convert_puzzle_text_to_list_of_lines(text)
+    print('*' * 80)
+    part_1_solution = solve_part_1(all_lines)
+    print('*' * 80)
+    print(f'Solution to day 2 part 1: {part_1_solution}')
+    print('*' * 80)
+
+
+
+
+def read_puzzle_input(filename):
+    with open(filename, 'r', encoding='utf-8') as puzzle_input:
+        return puzzle_input.read()
+
+def convert_puzzle_text_to_list_of_lines(text):
+    return text.split('\n')
+
+
+
+
+def solve_part_1(all_lines):
+    valid_passwords = 0
+    for index, line in enumerate(all_lines):
+        is_valid = check_password_for_validity(index, line)
+        if is_valid:
+            valid_passwords += 1
+    print(f'There are {valid_passwords} valid passwords in this file based on the independent password requirement for each password.')
+    return valid_passwords
+
+def check_password_for_validity(index, line):
+    try:
+        requirement, password      = line.split(':')
+        requirement                = requirement.strip()
+        password                   = password.strip()
+        number_of_times, character = requirement.split() # split by whitespace (should be one space character)
+        least, greatest            = [int(number) for number in number_of_times.split('-')]
+    except ValueError as error_message: # example -> ValueError: not enough values to unpack (expected 2, got 1)
+        print(f'Line {index} is improperly formatted. The exact message was:\n{error_message}')
+    except Exception as some_other_error:
+        print(f'There was an unexpected error when processing line {index}. The exact message was:\n{some_other_error}')
+    else:
+        actual_count_of_character  = password.count(character)
+        print(f'Password {index:>4} requires "{character}" to occur {least:>3} to {greatest:>3} times. Actual count: {actual_count_of_character}')
+        if least <= actual_count_of_character <= greatest:
+            return True
+    return False
+
+
+
+if __name__ == '__main__':
+    main()
