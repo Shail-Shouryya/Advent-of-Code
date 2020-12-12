46406d7a2cfb3f0cf52f47c664683dda87f7eeb6
Shail Shouryya <shailshouryya@gmail.com>
2020-12-11 19:15:16 -0800

    Make logging output from test more clear

--- 2020/day_1_test.py
+++ 2020/day_1_test.py
@@ -4,7 +4,7 @@ def main():
     part_1, part_2    = day_1.main()
     part_1_is_correct = part_1 == 388075
     part_2_is_correct = part_2 == 293450526
-    return f'Day 1 part 1 passes: {part_1_is_correct}\nDay 1 part 2 passes: {part_2_is_correct}'
+    return f'Day 1 part 1 answer is correct: {part_1_is_correct}\nDay 1 part 2 answer is correct: {part_2_is_correct}'
 
 if __name__ == '__main__':
     print(main())
