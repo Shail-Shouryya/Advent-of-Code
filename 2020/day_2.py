1c90e936f3f612533ea16f7bee71e067f3f51455
Shail Shouryya <shailshouryya@gmail.com>
2020-12-11 21:02:43 -0800

    Refactor duplicate code into parse_line() function

--- 2020/day_2.py
+++ 2020/day_2.py
@@ -68,6 +68,27 @@ def convert_puzzle_text_to_list_of_lines(text):
 
 
 
+def parse_line(index, line):
+    try:
+        requirement, password    = line.split(':')
+        requirement              = requirement.strip()
+        password                 = password.strip()
+        p_range, character       = requirement.split() # split by whitespace (should be one space character)
+        first, second = [int(number) for number in p_range.split('-')]
+    except ValueError as error_message: # example -> ValueError: not enough values to unpack (expected 2, got 1)
+        print(f'Line {index} is improperly formatted. The exact message was:\n{error_message}')
+    except Exception as some_other_error:
+        print(f'There was an unexpected error when processing line {index}. The exact message was:\n{some_other_error}')
+    else:
+        return password, character, first, second
+    # if we get here, this means there was some error,
+    # so we return the falsy value for the expected data type
+    # https://docs.python.org/3/library/stdtypes.html
+    return ('', '', 0, 0)
+
+
+
+
 def solve_part_1(all_lines):
     valid_passwords = 0
     for index, line in enumerate(all_lines):
@@ -78,22 +99,15 @@ def solve_part_1(all_lines):
     return valid_passwords
 
 def check_password_for_validity(index, line):
-    try:
-        requirement, password      = line.split(':')
-        requirement                = requirement.strip()
-        password                   = password.strip()
-        number_of_times, character = requirement.split() # split by whitespace (should be one space character)
-        least, greatest            = [int(number) for number in number_of_times.split('-')]
-    except ValueError as error_message: # example -> ValueError: not enough values to unpack (expected 2, got 1)
-        print(f'Line {index} is improperly formatted. The exact message was:\n{error_message}')
-    except Exception as some_other_error:
-        print(f'There was an unexpected error when processing line {index}. The exact message was:\n{some_other_error}')
-    else:
-        actual_count_of_character  = password.count(character)
-        print(f'Password {index:>4} requires "{character}" to occur {least:>3} to {greatest:>3} times. Actual count: {actual_count_of_character}')
-        if least <= actual_count_of_character <= greatest:
-            return True
-    return False
+    password, character, least, greatest = parse_line(index, line)
+    # python checks for falsy values when evaluating conditionals
+    # this is faster than performing
+    # password == ''
+    if not password:
+        return False
+    actual_count_of_character  = password.count(character)
+    print(f'Password {index:>4} requires "{character}" to occur {least:>3} to {greatest:>3} times. Actual count: {actual_count_of_character}')
+    return least <= actual_count_of_character <= greatest
 
 
 
@@ -109,40 +123,33 @@ def solve_part_2(all_lines):
 
 
 def check_password_for_validity_using_official_policy(index, line):
-    try:
-        requirement, password    = line.split(':')
-        requirement              = requirement.strip()
-        password                 = password.strip()
-        p_range, character       = requirement.split() # split by whitespace (should be one space character)
-        position_1, position_2 = [int(number) for number in p_range.split('-')]
-    except ValueError as error_message: # example -> ValueError: not enough values to unpack (expected 2, got 1)
-        print(f'Line {index} is improperly formatted. The exact message was:\n{error_message}')
-    except Exception as some_other_error:
-        print(f'There was an unexpected error when processing line {index}. The exact message was:\n{some_other_error}')
-    else:
-        position_1_is_valid = False
-        position_2_is_valid = False
-        password_length = len(password)
-        if 0 <= position_1 - 1 < password_length: position_1_is_valid = password[position_1 - 1] == character
-        if 0 <= position_2 - 1 < password_length: position_2_is_valid = password[position_2 - 1] == character
-        # ^ is the XOR operator, and the result is caluclated by
-        # flipping the bits at any position where the two arguments have a different value
-        # since position_1_is_valid and position_2_is_valid can only have the value of
-        # True or False in this scenario, the possible bit values for the arguments can only be
-        # either 0 (False) or 1 (True)
-        # 1 ^ 1 -> 0
-        # 1 ^ 0 -> 1
-        # 0 ^ 1 -> 1
-        # 0 ^ 0 -> 0
-        # since we want to determine if the password has the designated character appear
-        # only in ONE of the designated positions, performing
-        # position_1_is_valid XOR position_2_is_valid
-        # gives us exactly what we want
-        is_valid = position_1_is_valid ^ position_2_is_valid
-        print(f'Password {index:>4} requires "{character}" to occur at either position {position_1:>3} OR {position_2:>3} (but not at both nor neither positions): This is {is_valid}')
-        if is_valid:
-            return True
-    return False
+    password, character, position_1, position_2 = parse_line(index, line)
+    # python checks for falsy values when evaluating conditionals
+    # this is faster than performing
+    # password == ''
+    if not password:
+        return False
+    position_1_is_valid = False
+    position_2_is_valid = False
+    password_length = len(password)
+    if 0 <= position_1 - 1 < password_length: position_1_is_valid = password[position_1 - 1] == character
+    if 0 <= position_2 - 1 < password_length: position_2_is_valid = password[position_2 - 1] == character
+    # ^ is the XOR operator, and the result is caluclated by
+    # flipping the bits at any position where the two arguments have a different value
+    # since position_1_is_valid and position_2_is_valid can only have the value of
+    # True or False in this scenario, the possible bit values for the arguments can only be
+    # either 0 (False) or 1 (True)
+    # 1 ^ 1 -> 0
+    # 1 ^ 0 -> 1
+    # 0 ^ 1 -> 1
+    # 0 ^ 0 -> 0
+    # since we want to determine if the password has the designated character appear
+    # only in ONE of the designated positions, performing
+    # position_1_is_valid XOR position_2_is_valid
+    # gives us exactly what we want
+    is_valid = position_1_is_valid ^ position_2_is_valid
+    print(f'Password {index:>4} requires "{character}" to occur at either position {position_1:>3} OR {position_2:>3} (but not at both nor neither positions): This is {is_valid}')
+    return is_valid
 
 
 
